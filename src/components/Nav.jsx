2e40cca83cc5db62873e998801aff82064b71909
Peeyush Kumar <24976633+PeeyushKumar@users.noreply.github.com>
2021-10-14 14:26:42 +0530

    nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+
+const Nav = () => {
+
+    return (
+        <div className="nav">
+            <h1 className="logo">Ideas</h1>
+            <ul className="nav-list">
+               <li><a href="#">One</a></li>
+               <li><a href="#">Two</a></li> 
+            </ul>
+        </div>
+    );
+}
+
+export default Nav;
