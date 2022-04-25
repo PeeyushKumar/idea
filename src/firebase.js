070cccc237b3c16f0b6f5904f44a616db819c6a7
PeeyushKumar <peeyshkumar.25oct@gmail.com>
2022-04-26 02:16:42 +0530

    changed firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBwm3dEj3g2r1zCg1cMAi9FS8BeibVAzpM",
-    authDomain: "peeyush-project.firebaseapp.com",
-    projectId: "peeyush-project",
-    storageBucket: "peeyush-project.appspot.com",
-    messagingSenderId: "15254799484",
-    appId: "1:15254799484:web:22744b8ebd0e9df24ff5f9"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 
 
 initializeApp(firebaseConfig);
